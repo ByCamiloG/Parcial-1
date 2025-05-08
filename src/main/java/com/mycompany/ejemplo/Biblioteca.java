28075be3fad9678727032d4b66c2744b6dacd0e9
jaimeramon-unal git config --global user.email <you@example.com>
2025-05-08 15:50:35 -0500

    Version 2

---
+++ src/main/java/com/mycompany/ejemplo/Biblioteca.java
@@ -0,0 +1,29 @@
+
+package com.mycompany.ejemplo;
+
+public class Biblioteca {
+    public String registro;
+    public String Categoria;
+
+
+public Biblioteca(String registro, String Categoria) {
+        this.registro = registro;
+        this.Categoria = Categoria;
+    }
+    
+       // Getters
+    public String getregistro() {
+        return registro;
+    }
+
+    public String getCategoria() {
+        return Categoria;
+    }
+
+    
+}
+
+
+
+    
+
